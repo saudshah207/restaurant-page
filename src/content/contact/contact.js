3df9b234042f9a000173d7202bee805654caa2ae
saud <121959804+saudshah207@users.noreply.github.com>
2026-07-16 19:13:40 +0500

    Add contact content

--- src/content/contact/contact.js
+++ src/content/contact/contact.js
@@ -1,3 +1,46 @@
+import "./contact.css";
+
+function getInputField(id, type) {
+  let input;
+
+  if (type) {
+    input = document.createElement("input");
+    input.type = type;
+  } else input = document.createElement("textarea");
+
+  input.name = id;
+  input.id = id;
+
+  return input;
+}
+
+function getFormSection(label, id, inputType = null) {
+  const labelElement = document.createElement("label"),
+    labelTextElement = document.createElement("span");
+
+  labelTextElement.classList.add("label-text");
+  labelTextElement.textContent = label;
+
+  labelElement.htmlFor = id;
+
+  const inputElement = getInputField(id, inputType);
+  labelElement.append(labelTextElement, inputElement);
+
+  return labelElement;
+}
+
 export default function () {
-    return "Contact";
+  const form = document.createElement("form");
+
+  const nameSection = getFormSection("Name", "name", "text"),
+    emailSection = getFormSection("Email", "email", "email"),
+    messageSection = getFormSection("Message", "message"),
+    submitButton = document.createElement("button");
+
+  submitButton.textContent = "Submit";
+  submitButton.classList.add("button");
+
+  form.append(nameSection, emailSection, messageSection, submitButton);
+
+  return form;
 }
