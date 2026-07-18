a2d14294d52fd0af9594722a8f5ac6ad62c1238b
saud <121959804+saudshah207@users.noreply.github.com>
2026-07-18 17:48:49 +0500

    Make webpack-dev-server watch for changes to our template.html file

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,4 +4,7 @@ import { merge } from "webpack-merge";
 export default merge(common, {
   mode: "development",
   devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
 });
